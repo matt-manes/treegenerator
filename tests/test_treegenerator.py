c886d8becf71de1ed9af1b7be2fdf17259cf6ae1
matt manes <mattmanes@pm.me>
2023-01-21 20:12:17 -0600

    Changed naming to be PEP compliant.

---
+++ tests/test_treegenerator.py
@@ -0,0 +1,39 @@
+import pytest
+
+from treegenerator import TreeGenerator, UrlTreeGenerator
+
+
+def test_treegenerator_generate():
+    ...
+
+
+def test_treegenerator__drop_parents():
+    ...
+
+
+def test_treegenerator___str__():
+    ...
+
+
+def test_treegenerator__crawl():
+    ...
+
+
+def test_treegenerator__generate_tree():
+    ...
+
+
+def test_treegenerator__format_branch_name():
+    ...
+
+
+def test_treegenerator__convert_branch_to_string():
+    ...
+
+
+def test_treegenerator__trim_tree():
+    ...
+
+
+def test_treegenerator__generate_tree_string():
+    ...
